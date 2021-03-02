4a11f7822928e1d355dc69f5b28cdebfdbe48291
user <60804817+ichg4354@users.noreply.github.com>
2021-03-02 19:56:30 +0900

    learn how to make a func that does print hello world

--- index.cpp
+++ index.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <iostream> //전처리 지시자
 
-using namespace std;
+using namespace std; //종결자 역할
 
-int main()
+int main() // c++ 실행시킬 때 무조건 있어야 하는 함수 이름
 {
     cout << "HELLO WORLD" << endl;
     return 0;
